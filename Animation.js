1bbda2b879cf1566a4a42c3e462fe6465ba5876c
VCRISff <Victus50w1@gmail.com>
2025-06-14 22:34:09 -0400

    Update Animation.js

--- Animation.js
+++ Animation.js
@@ -84,8 +84,8 @@
             if (dist < maxDist && (isTouching || !('ontouchstart' in window))) {
                 const force = (maxDist - dist) / maxDist;
                 const angle = Math.atan2(dy, dx);
-                p.x = p.baseX - Math.cos(angle) * force * 20;
-                p.y = p.baseY - Math.sin(angle) * force * 20;
+                p.x = p.baseX - Math.cos(angle) * force * 5;
+                p.y = p.baseY - Math.sin(angle) * force * 5;
 
                 ctx.fillStyle = p.scatteredColor;
             } else {
